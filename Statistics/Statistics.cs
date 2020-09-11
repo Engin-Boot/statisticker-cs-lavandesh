3ddcd5c82d13105916884e285b5bdfc583c27a8b
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-09-11 12:37:29 +0530

    Initial Commit - added implimentation

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,79 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        private double avg(List<double> numbers)
+        {
+            double sum = default(double);
+            for (int i=0; i<numbers.Count; i++)
+            {
+                sum+= numbers[i];
+            }
+            return (double)sum/numbers.Count;
         }
+        private double min_num(List<double> numbers)
+        {
+            double min = numbers[0];
+            for (int i=0; i<numbers.Count; i++)
+            {
+                if (min > numbers[i])
+                {
+                    min=numbers[i];
+                }
+            }
+            return min;
+        }
+        private double max_num(List<double> numbers)
+        {
+            double max = numbers[0];
+            for (int i=0; i<numbers.Count; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max=numbers[i];
+                }
+            }
+            return max;
+        }
+        
+        public Stats CalculateStatistics(List<double> numbers)
+        {
+            Stats obj = new Stats();
+            if (numbers.Count==0)
+                return obj;
+            obj.Average = avg(numbers);
+            obj.Min = min_num(numbers);
+            obj.Max = max_num(numbers);
+            return obj;
+        }
+    }
+    public class Stats
+    {
+        private double average, min, max;
+        internal Stats()
+        {
+            this.average=double.NaN;
+            this.min  = double.NaN;
+            this.max = double.NaN;
+        }
+        #region Properties
+        public double Average
+        {
+            get;
+            set;
+        }
+        public double Min
+        {
+            get;
+            set;
+        }
+        public double Max
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+
     }
 }
